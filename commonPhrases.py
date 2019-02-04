0415c805b1ca6c7f40ef8925e26ae28095a0735a
lukamiletic95 <lukapn@gmail.com>
2019-02-04 07:40:42 -0800

    Common phrases updated!

--- commonPhrases.py
+++ commonPhrases.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-toSkip = ['and', 'or', 'in', 'the', 'a', 'an', 'is', 'on', 'under', 'of', 'to', 'but']
+toSkip = ['and', 'or', 'in', 'the', 'a', 'an', 'is', 'on', 'under', 'of', 'to', 'but', 'its', 'with']
 characters = [',', '(', ')', '[', ']', ':', '?', '!', '.']
 
 sheet = pd.read_csv(r"files/pubmed_title_year.csv")
@@ -16,17 +16,23 @@ for i in range(len(sheet['Title'])):
 
     words = title.split(' ')
 
-    newWord = ""
+    newWord = ''
     for word in words:
+        word = word.lower()
+
         if word in toSkip:
-            newWordLower = "".join(newWord.split()).lower()
+            if newWord == '':
+                continue
 
-            if newWordLower not in phraseToName:
-                phraseToName[newWordLower] = newWord
-                phraseToCount[newWordLower] = 1
+            newWordKey = ''.join(newWord.split())
+
+            if newWordKey not in phraseToName:
+                phraseToName[newWordKey] = newWord
+                phraseToCount[newWordKey] = 1
             else:
-                phraseToCount[newWordLower] += 1
+                phraseToCount[newWordKey] += 1
 
+            newWord = ''
         else:
             newWord += ' ' + word
 
@@ -36,6 +42,7 @@ phraseToCount = {k: v for k, v in
 file = open('files/commonPhrases.txt', 'w+')
 
 for key, value in phraseToCount.items():
-    file.write(phraseToName[key] + ': ' + str(value) + '\n')
+    if len(phraseToName[key].split()) > 1:
+        file.write(phraseToName[key] + ': ' + str(value) + '\n')
 
 file.close()
