54ea4b67fffd5c149ba29fbf60ed504b77f8d84a
lukamiletic95 <lukapn@gmail.com>
2019-02-05 03:56:51 -0800

    Top three journals and top three genres added as nodes' attributes.

--- collaborationNetwork.py
+++ collaborationNetwork.py
@@ -25,7 +25,7 @@ for authorList in sheet['Authors']:
                 G.add_edge(authors[i], authors[j])
                 G[authors[i]][authors[j]]['weight'] = 1 / (len(set(authors)) - 1)
 
-# adding Attribute
+# adding attribute for journals
 
 authorsDictionary = {}
 sheetJournals = pd.read_csv(r"files/pubmed_journals.csv")
@@ -60,14 +60,79 @@ for author, journals in authorsDictionary.items():
     attributes = {author: {'topJournal': topJournal}}
     nx.set_node_attributes(G, attributes)
 
-# adding Attribute
+# adding attribute for journals
 
-#nx.write_gml(G, "files/collaborationNetwork.gml")  # for writing into GML file
+# adding attribute for phrases
+
+toSkip = ['and', 'or', 'in', 'the', 'a', 'an', 'is', 'on', 'under', 'of', 'to', 'but', 'its', 'with', 'for', 'by', 'as', 'via', 'at']
+characters = [',', '(', ')', '[', ']', ':', '?', '!', '.']
+
+sheetAuthors = pd.read_csv(r"files/pubmed_result.csv")
+sheetTitle = pd.read_csv(r"files/pubmed_title_year.csv")
+
+phraseToName = {}
+phrasesPerAuthor = {}
+
+for i in range(len(sheetTitle['Title'])):
+    title = sheetTitle['Title'][i]
+    authors = sheetAuthors['Authors'][i].split(",")
+
+    for i in range(len(authors)):
+        authors[i] = authors[i].split(";")[0].strip().replace(".", "")
+
+    if "et al" in authors:
+        authors.remove("et al")
+
+    authors = set(authors)
+
+    for author in authors:
+        if author not in phrasesPerAuthor:
+            phrasesPerAuthor[author] = {}
+
+    for character in characters:
+        title = title.replace(character, '')
+
+    words = title.split(' ')
+
+    newWord = ''
+    numWords = 0
+    for word in words:
+        word = word.lower()
+
+        if word in toSkip:
+            continue
+        else:
+            numWords += 1
+            newWord += ' ' + word
+
+            if numWords == 2:
+                newWordKey = ''.join(newWord.split())
+
+                if newWordKey not in phraseToName:
+                    phraseToName[newWordKey] = newWord
+
+                for author in authors:
+                    if newWordKey not in phrasesPerAuthor[author]:
+                        phrasesPerAuthor[author][newWordKey] = 1
+                    else:
+                        phrasesPerAuthor[author][newWordKey] += 1
+
+                newWord = newWord.split(' ')[1]
+                numWords = 1
+
+for key in phrasesPerAuthor.keys():
+    phrasesPerAuthor[key] = {k: v for k, v in
+                                sorted(phrasesPerAuthor[key].items(), key=lambda x: x[1], reverse=True)}
+
+# adding attribute for phrases
+
+nx.write_gml(G, "files/collaborationNetwork.gml")  # for writing into GML file
 
 nodeData = {
     'Id': [],
     'Label': [],
-    'TopJournal': []
+    'TopJournal': [],
+    'TopPhrase': []
 }
 
 labelToId = {}
@@ -76,11 +141,26 @@ nodeLabels = list(G.nodes)
 for i in range(len(nodeLabels)):
     nodeData['Id'].append(i)
     nodeData['Label'].append(nodeLabels[i])
-    nodeData['TopJournal'].append(list(authorsDictionary[nodeLabels[i]].keys())[0])
+
+    topJournals = list(authorsDictionary[nodeLabels[i]].keys())
+    topJournalsTxt = ""
+    for j in range(len(topJournals)):
+        topJournalsTxt += topJournals[j] + " || "
+        if j == 2:
+            break
+    nodeData['TopJournal'].append(topJournalsTxt)
+
+    topPhrases = list(phrasesPerAuthor[nodeLabels[i]].keys())
+    topPhrasesTxt = ""
+    for j in range(len(topPhrases)):
+        topPhrasesTxt += phraseToName[topPhrases[j]] + " || "
+        if j == 2:
+            break
+    nodeData['TopPhrase'].append(topPhrasesTxt)
 
     labelToId[nodeLabels[i]] = i  # for creating the edges csv file
 
-nodeDataFrame = pd.DataFrame(nodeData, columns=['Id', 'Label', 'TopJournal'])
+nodeDataFrame = pd.DataFrame(nodeData, columns=['Id', 'Label', 'TopJournal', 'TopPhrase'])
 nodeDataFrame.to_csv("files/node.csv", index=False)
 
 edgeData = {
